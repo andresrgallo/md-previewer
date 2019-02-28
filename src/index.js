fb242d444e1ba23d4f235f99af85600f1803cbc1
andresrgallo <andresgallo@hotmail.com>
2019-02-28 20:00:23 +1100

    Add service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 
 import 'index.scss';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('/service-worker.js')
+			.then(registration => {
+				console.log('SW registered: ', registration);
+			})
+			.catch(registrationError => {
+				console.log('SW registration failed: ', registrationError);
+			});
+	});
+}
+
 let HelloWorld = () => {
 	return <h1>Hello there World!</h1>;
 };
